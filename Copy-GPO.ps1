a2e792dc0d6efb8f698f5c37918ca12703a30eed
Stuart Chapman <stuartpchapman@hotmail.com>
2016-02-10 11:37:03 +0800

    Create Copy-GPO.ps1

---
+++ Copy-GPO.ps1
@@ -0,0 +1,3 @@
+Copy-GPO [-SourceName] <string> -TargetName <string> [-CopyACL] [-MigrationTable <string>] [-SourceDomain <string>] [-SourceDomainController <string>] [-TargetDomain <string>] [-TargetDomainController <string>] [-Confirm]
+
+Copy-GPO -SourceName "source-gpo" -TargetName "target-gpo" -CopyACL
